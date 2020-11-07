cbbd86b37f30751ac856b5d1afc3a75e31033c24
myfrenKvr0 <64550515+myfrenKvr0@users.noreply.github.com>
2020-11-07 19:04:06 +0200

    Update prepare-documents.js

--- actions/prepare-documents.js
+++ actions/prepare-documents.js
@@ -2,7 +2,7 @@
  * Prepare the customer repository entry to be persisted
  */
 function main(params) {
-  if (!params.name || !params.comment) {
+  if (!params.name || !params.surname || !params.id|| !params.gender || !params.drivers_license || !params.contact_number || !params.address || !params.vehicle_make || !params.vehicle_series_name || !params.license_plate || !params.colour || !params.year) {
     return Promise.reject({ error: 'all the fields have not been filled.'});
   }
 
@@ -12,6 +12,7 @@ function main(params) {
        name: params.name,
        surname: params.surname,
        id: params.id,
+       gender: params.gender,
        drivers_license: params.drivers_license,
        contact_number: params.contact_number,
        address: params.address,
