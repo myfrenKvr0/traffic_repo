41c04666d29fc9a477b987b2e33ff1d3ef254098
myfrenKvr0 <64550515+myfrenKvr0@users.noreply.github.com>
2020-11-10 19:16:58 +0200

    Update repository.js

--- docs/repository.js
+++ docs/repository.js
@@ -10,91 +10,91 @@ const repository = {
       url: `${apiUrl}/entries`,
       dataType: 'json'
     });
-  },
+		},
   // add a single repository entry
-  add(name, surname, id, gender, drivers_license, contact_number, address, vehicle_make, vehicle_series_name, license_plate, colour, year) {
-    console.log('Sending',name, surname, id, gender, drivers_license, contact_number, address, vehicle_make, vehicle_series_name, license_plate, colour, year)
-    return $.ajax({
-      type: 'PUT',
-      url: `${apiUrl}/entries`,
-      contentType: 'application/json; charset=utf-8',
-      data: JSON.stringify({
+	add(name, surname, id, gender, drivers_license, contact_number, address, vehicle_make, vehicle_series_name, license_plate, colour, year) {
+		console.log('Sending',name, surname, id, gender, drivers_license, contact_number, address, vehicle_make, vehicle_series_name, license_plate, colour, year)
+		return $.ajax({
+		type: 'PUT',
+		url: `${apiUrl}/entries`,
+		contentType: 'application/json; charset=utf-8',
+		data: JSON.stringify({
         	name,
-		surname,
-		id,
-		gender,
-		drivers_license,
-		contact_number,
-		address, vehicle_make,
-		vehicle_series_name,
-		license_plate,
-		colour, 
-		year,
-      }),
-      dataType: 'json',
+			surname,
+			id,
+			gender,
+			drivers_license,
+			contact_number,
+			address, vehicle_make,
+			vehicle_series_name,
+			license_plate,
+			colour, 
+			year,
+			}),
+			dataType: 'json',
     });
-  }
+}
 };
 
 (function() {
 
-  let entriesTemplate;
+	let entriesTemplate;
 
-  function prepareTemplates() {
-    entriesTemplate = Handlebars.compile($('#entries-template').html());
-  }
-
-  // retrieve entries and update the UI
-  function loadEntries() {
-    console.log('Loading entries...');
-    $('#entries').html('Loading entries...');
-    repository.get().done(function(result) {
-      if (!result.entries) {
-        return;
-      }
-
-      const context = {
-        entries: result.entries
-      }
-      $('#entries').html(entriesTemplate(context));
-    }).error(function(error) {
-      $('#entries').html('No entries');
-      console.log(error);
-    });
-  }
-
-  // intercept the click on the submit button, add the guestbook entry and
-  // reload entries on success
-  $(document).on('submit', '#addEntry', function(e) {
-    e.preventDefault();
-    if($('#name').length>0){
-    	if (confirm ("Customer with the same Name and Surname already exists. Continue to store information?")){
-		alert("Thank you for confirming");
-		
-		repository.add(
-		$('#name').val().trim(),
-		$('#surname').val().trim(),
-		$('#id').val().trim(),
-		$('#gender').val().trim(),
-		$('#drivers_license').val().trim(),
-		$('#contact_number').val().trim(),
-		$('#address').val().trim(),
-		$('#vehicle_make').val().trim(),
-		$('#vehicle_series_name').val().trim(),
-		$('#license_plate').val().trim(),
-		$('#colour').val().trim(),
-		$('#year').val().trim()
-	    	).done(function(result) {
-	      	// reload entries
-	      	loadEntries();
-	    	}).error(function(error) {
-	      	console.log(error);
-	    	});
+	function prepareTemplates() {
+		entriesTemplate = Handlebars.compile($('#entries-template').html());
 	}
-	else{
-		alert("Information will not be stored")
+
+	// retrieve entries and update the UI
+	function loadEntries() {
+		console.log('Loading entries...');
+		$('#entries').html('Loading entries...');
+		repository.get().done(function(result) {
+			if (!result.entries) {
+				return;
+			}
+
+			const context = {
+				entries: result.entries
+			}
+		$('#entries').html(entriesTemplate(context));
+		}).error(function(error) {
+			$('#entries').html('No entries');
+			console.log(error);
+		});
 	}
-    }
+
+	// intercept the click on the submit button, add the guestbook entry and
+	// reload entries on success
+	$(document).on('submit', '#addEntry', function(e) {
+		e.preventDefault();
+		if(repository["id"] == ($('#id'))){
+			if (confirm ("Customer with the same ID already exists. Continue to store information?")){
+				alert("Thank you for confirming");
+				
+				repository.add(
+				$('#name').val().trim(),
+				$('#surname').val().trim(),
+				$('#id').val().trim(),
+				$('#gender').val().trim(),
+				$('#drivers_license').val().trim(),
+				$('#contact_number').val().trim(),
+				$('#address').val().trim(),
+				$('#vehicle_make').val().trim(),
+				$('#vehicle_series_name').val().trim(),
+				$('#license_plate').val().trim(),
+				$('#colour').val().trim(),
+				$('#year').val().trim()
+					).done(function(result) {
+					// reload entries
+					loadEntries();
+					}).error(function(error) {
+					console.log(error);
+					});
+		}
+			else{
+				alert("Information will not be stored")
+			}
+		}
      else{
 		 repository.add(
 		 $('#name').val().trim(),
